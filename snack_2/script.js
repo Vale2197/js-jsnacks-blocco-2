55bb6e02067a9a7aa7b855a16218baa7ca2c5ae6
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-25 12:38:27 +0200

    aggiunto punti random squadra

--- snack_2/script.js
+++ snack_2/script.js
@@ -1,36 +1,47 @@
-let nome = document.querySelector(".txt").value;
-
-console.log(nome);
 
 let squadre = [
     {
-        nome,
+        nome: "squadraUno",
         puntiFatti: 0,
         falliSubiti: 0,
     },
     {
-        nome,
+        nome: "squadraDue",
         puntiFatti: 0,
         falliSubiti: 0,
     },
     {
-        nome,
+        nome: "squadraTre",
         puntiFatti: 0,
         falliSubiti: 0,
     },
     {
-        nome,
+        nome: "squadraQuattro",
         puntiFatti: 0,
         falliSubiti: 0,
     },
     {
-        nome,
+        nome: "squadraCinque",
         puntiFatti: 0,
         falliSubiti: 0,
     },
     {
-        nome,
+        nome: "squadraSei",
         puntiFatti: 0,
         falliSubiti: 0,
     }
-]
+];
+
+
+for (let i = 0; i < squadre.length; i++) {
+    let squadra = squadre[i];
+
+    if (squadra.falliSubiti || squadra.puntiFatti == 0) {
+        squadra.falliSubiti = Math.floor(Math.random() * 100);
+        squadra.puntiFatti = Math.floor(Math.random() * 100);
+    }
+    
+    console.log(squadra);
+}
+
+/* Math.floor(Math.random() * 100); */
